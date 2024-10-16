66d28c8a6fb8b60afa82aeb2a86ddf2c272b45ae
kwangho <>
2024-10-16 13:44:15 +0900

    init

---
+++ src/main/java/com/ks/gateway/KsSpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ks.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsSpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsSpringGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ks/gateway/KsSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsSpringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
